0b16de4f42cd14e9e12954fb202e6aa101c7aa83
EduardaDuDu <17m.eduarda@gmail.com>
2026-02-27 11:36:40 -0300

    app.js

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
+const ctx = document.getElementById("grafico");
 
+new Chart(ctx, {
+  type: "doughnut",
+  data: {
+    labels: ["Educação", "Alimentação", "Transporte", "Academia"],
+    datasets: [{
+      data: [500, 300, 230, 120],
+      backgroundColor: [
+        "#7b2ff7",
+        "#ff6384",
+        "#36a2eb",
+        "#ffcd56"
+      ]
+    }]
+  }
+});
