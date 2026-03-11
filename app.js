adea82217dc4fab2deff4dd5f18c5b3a9048a73a
EduardaDuDu <17m.eduarda@gmail.com>
2026-03-11 04:00:58 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-let grafico;
-
 let categorias = [
 {nome:"Educação",valor:0},
 {nome:"Alimentação",valor:0},
@@ -8,40 +6,38 @@ let categorias = [
 {nome:"Outros",valor:0}
 ];
 
+let grafico;
 
 document.addEventListener("DOMContentLoaded",()=>{
 
+// mês automático
 const hoje = new Date();
 
-const meses=[
+const meses = [
 "Janeiro","Fevereiro","Março","Abril","Maio","Junho",
 "Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"
 ];
 
-document.querySelector(".mes").innerText =
-meses[hoje.getMonth()]+" "+hoje.getFullYear();
+document.getElementById("mes").innerText =
+meses[hoje.getMonth()] + " " + hoje.getFullYear();
 
 showTab("dashboard");
 
 criarGrafico();
 
-atualizarInterface();
+atualizarLista();
 
 });
 
-
-
 function showTab(id){
 
-document.querySelectorAll(".tab").forEach(tab=>{
-tab.style.display="none";
+document.querySelectorAll(".tab").forEach(t=>{
+t.style.display="none";
 });
 
 document.getElementById(id).style.display="block";
 
 }
-
-
 
 function criarGrafico(){
 
@@ -52,65 +48,45 @@ grafico = new Chart(ctx,{
 type:"doughnut",
 
 data:{
-
 labels:categorias.map(c=>c.nome),
 
 datasets:[{
-
-data:categorias.map(c=>c.valor),
-
-backgroundColor:[
-"#7b2ff7",
-"#ff6384",
-"#36a2eb",
-"#ffcd56",
-"#4bc0c0"
-]
-
+data:categorias.map(c=>c.valor)
 }]
-
 }
 
 });
 
 }
 
+function atualizarLista(){
 
-
-function atualizarInterface(){
-
-const lista=document.getElementById("listaCategorias");
+const lista = document.getElementById("lista");
 
 lista.innerHTML="";
 
 categorias.forEach((cat,i)=>{
 
-const li=document.createElement("li");
+const li = document.createElement("li");
 
-li.innerText=cat.nome+" — R$ "+cat.valor;
+li.innerText = cat.nome + " - R$ " + cat.valor;
 
-li.onclick=()=>editarValor(i);
+li.onclick = ()=>editar(i);
 
 lista.appendChild(li);
 
 });
 
-grafico.data.datasets[0].data=
+grafico.data.datasets[0].data =
 categorias.map(c=>c.valor);
 
 grafico.update();
 
-const total= categorias.reduce((s,v)=>s+v.valor,0);
-
-document.getElementById("totalDespesas").innerText="R$ "+total;
-
 }
 
+function editar(i){
 
-
-function editarValor(index){
-
-let valor = prompt("Digite o valor para "+categorias[index].nome);
+let valor = prompt("Digite valor para "+categorias[i].nome);
 
 if(valor===null) return;
 
@@ -118,8 +94,8 @@ valor = Number(valor);
 
 if(isNaN(valor)) return;
 
-categorias[index].valor=valor;
+categorias[i].valor = valor;
 
-atualizarInterface();
+atualizarLista();
 
 }
